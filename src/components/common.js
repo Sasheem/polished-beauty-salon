a1f8b8330e6b7e4c585766eb2d137e319da82663
Sasheem Snell <sasheem.snell@gmail.com>
2021-05-03 13:45:26 -0400

    added text components

---
+++ src/components/common.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const Button = styled.div`
+    padding: 0.2rem;
+    display: flex;
+    justify-content: center;
+    border-radius: 5%;
+    color: white;
+    background-color: #FF99AC;
+`
+export const Heading1 = styled.h1`
+    color: #44566C;
+    font-size: 36px;
+`
+export const Heading2 = styled.h2`
+    color: #44566C;
+    font-size: 30px;
+`
+export const Heading3 = styled.h3`
+    color: #44566C;
+    font-size: 24px;
+`
+export const Heading4 = styled.h4`
+    color: #44566C;
+    font-size: 20px;
+`
+export const Heading4LC = styled(Heading4)`
+    color: #8A97A6;
+`
+export const SubHeading = styled.p`
+    color: #44566C;
+    font-size: 18px;
+`         
+export const BodyText1 = styled.p`
+    color: #44566C;
+    font-size: 16px;
+`
+export const BodyText2 = styled.p`
+    color: #44566C;
+    font-size: 14px;
+`
+export const BodyText2LC = styled(BodyText2)`
+    color: #8996A6;
+`
+export const BodyText3 = styled.p`
+    color: #44566C;
+    font-size: 12px;
+`
+export const BodyText3LC = styled(BodyText3)`
+    color: #8996A6;
+`
