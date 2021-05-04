23b3df6081f0f4c07e3137e8538822ae05f01935
Sasheem Snell <sasheem.snell@gmail.com>
2021-05-04 01:15:57 -0400

    moved to separate component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
 
 // local components
-import { Heading1, SubHeading } from '../components/common'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Banner from '../components/banner'
 import Services from '../components/services'
 
 // styled components
-const HeadingStyled = styled.h1``
-const HeadingAccent = styled.p``
 const Section = styled.section`
-  padding-top: 256px;
-  padding-bottom: 304px;
-
+  // padding-top: 256px;
+  // padding-bottom: 304px;
+  
   // active when browser window is 600px or smaller
   @media only screen and (max-width: 600px) {
     margin-top: 128px;
@@ -23,22 +22,14 @@ const Section = styled.section`
 `
 
 // markup
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <Section>
-      <Heading1>
-        Hi my name is Alina {' '}
-        <span role='img' aria-label='Manicure emoji'>
-          💅
-        </span>
-      </Heading1>
-      <SubHeading>
-        Nailtech located in Coral Springs, FL.{' '}
-      </SubHeading>
-    </Section>
-    <Services />
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <Banner />
+      <Services />
+    </Layout>
+  )
+}
 
 export default IndexPage
