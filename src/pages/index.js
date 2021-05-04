35d0d0cbb8a26907848f37fa7c7b27ba6aa97d89
Sasheem Snell <sasheem.snell@gmail.com>
2021-05-04 01:17:29 -0400

    removed old code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,11 @@
 import * as React from 'react'
-import { useStaticQuery, graphql, Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
-import BackgroundImage from 'gatsby-background-image'
 
 // local components
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Banner from '../components/banner'
 import Services from '../components/services'
-
-// styled components
-const Section = styled.section`
-  // padding-top: 256px;
-  // padding-bottom: 304px;
-  
-  // active when browser window is 600px or smaller
-  @media only screen and (max-width: 600px) {
-    margin-top: 128px;
-  }
-`
 
 // markup
 const IndexPage = () => {
